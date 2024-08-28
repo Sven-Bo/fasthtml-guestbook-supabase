f131d9ce5708a4c10cfcb97c43d5b08c216f748e
Sven <contact@pythonandvba.com>
2024-08-28 20:33:45 +0300

    Format timestamp

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ load_dotenv()
 # Constants for input character limits and timestamp format
 MAX_NAME_CHAR = 15
 MAX_MESSAGE_CHAR = 50
+TIMESTAMP_FMT = "%Y-%m-%d %I:%M:%S %p CET"
 
 # Initialize Supabase client
 supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
